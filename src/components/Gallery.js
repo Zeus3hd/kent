9db927e041fd379dcd0539533f26a498ae59594a
unknown <dafuq.ahd@gmail.com>
2019-09-07 13:19:21 +0300

    Add Filter div to Gallery Component, changed gallery component css position to relative

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,6 +3,7 @@ import testImage from "../imgs/1.jpg";
 import testImage2 from "../imgs/2.jpg";
 import testImage3 from "../imgs/3.jpg";
 import GalleryCard from "./GalleryCard";
+import filterIcon from "../imgs/filter.png";
 const Gallery = () => {
   return (
     <div id="gallery">
@@ -11,6 +12,9 @@ const Gallery = () => {
       <GalleryCard testImage={testImage3} />
       <GalleryCard testImage={testImage} />
       <GalleryCard testImage={testImage} />
+      <div id="filter">
+        <img src={filterIcon} alt="filter" />
+      </div>
     </div>
   );
 };
